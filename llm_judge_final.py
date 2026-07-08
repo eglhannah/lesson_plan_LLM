aa5a5f905ddcd7fa49a80aed743c44c1828f9a19
Hannah Egl <hde7cm@virginia.edu>
2026-07-08 11:42:09 -0400

    align judge file and inference file

--- llm_judge_final.py
+++ llm_judge_final.py
@@ -14,6 +14,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import pandas as pd
 import csv
+from inference_final import assign_grade_level, outputs, response, it_response
 
 # Import CSV of prompts and response
 outputs = pd.read_csv("outputs.csv")
